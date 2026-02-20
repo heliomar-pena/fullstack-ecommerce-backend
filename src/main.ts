bc9c52e572cdea69a300f2e875f957cae9793b6d
Heliomar Pena <penaheliomar@gmail.com>
2026-02-20 20:40:59 -0300

    fix: added swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('e-commerce')
+    .setDescription('A e-commerce built with NestJS')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();

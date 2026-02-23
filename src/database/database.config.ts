6800f0d76c6b47a8190a31eaae3a5ed3b9cecfdb
Heliomar Pena <penaheliomar@gmail.com>
2026-02-22 23:00:32 -0300

    feat: added new db config and removed husky from prepare

--- src/database/database.config.ts
+++ src/database/database.config.ts
@@ -1,6 +1,7 @@
 import { registerAs } from '@nestjs/config';
 
 const databaseConfig = registerAs('database', () => ({
+  url: process.env.DATABASE_URL,
   host: process.env.DATABASE_HOST,
   port: process.env.DATABASE_PORT
     ? Number(process.env.DATABASE_PORT)
--- src/database/utils/generate-data-source-config.ts
+++ src/database/utils/generate-data-source-config.ts
@@ -4,22 +4,30 @@ import { DataSourceOptions } from 'typeorm/browser';
 
 const generateDataSourceConfig = (
   configuration: ConfigType<typeof databaseConfig>,
-): DataSourceOptions => ({
-  type: 'postgres',
-  host: configuration.host,
-  port: configuration.port,
-  username: configuration.username,
-  password: configuration.password,
-  database: configuration.database,
-  entities: [__dirname + '/../../**/*.entity{.ts,.js}'],
-  synchronize: configuration.syncronize,
-  migrations: configuration.migrations,
-  migrationsTableName: 'migrations',
-  ...(configuration.enable_ssl_configuration && {
-    ssl: {
-      rejectUnauthorized: configuration.ssl_reject_unauthorized,
-    },
-  }),
-});
+): DataSourceOptions => {
+  const dbConnection = configuration.url
+    ? { url: configuration.url }
+    : {
+        host: configuration.host,
+        port: configuration.port,
+        username: configuration.username,
+        password: configuration.password,
+        database: configuration.database,
+      };
+
+  return {
+    type: 'postgres',
+    ...dbConnection,
+    entities: [__dirname + '/../../**/*.entity{.ts,.js}'],
+    synchronize: configuration.syncronize,
+    migrations: configuration.migrations,
+    migrationsTableName: 'migrations',
+    ...(configuration.enable_ssl_configuration && {
+      ssl: {
+        rejectUnauthorized: configuration.ssl_reject_unauthorized,
+      },
+    }),
+  };
+};
 
 export default generateDataSourceConfig;
